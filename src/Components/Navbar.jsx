dc98146f73a489b7286fe788fbf560059374ea40
Gowthamlogicsolver <gowtham2004123@gmail.com>
2024-09-24 13:47:47 +0530

    kct image

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({ scrollToSection, HomeRef, AboutRef, TeamsRef, FixturesRef }) =
     <div className='MainNavContainer'>
       <nav className="nav-class">
         <div className="clgLogo">
-          <img src="/kct.png" alt="College Logo" />
+          <img src="D:\\a-a-kalam\\KalamSite\\KalamSite\\public\\kct.png" alt="College Logo" />
         </div>
 
         {/* Regular Navigation Items for larger screens */}
